735a48afe4d11bc0eca8cc2de78c187022c8a8e9
Will Wheeler <willwheelera@gmail.com>
2018-07-18 19:01:51 -0500

    Uncomment import CifParser

--- autopyscf.py
+++ autopyscf.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import os
 import shutil as sh
-#from pymatgen.io.cif import CifParser
+from pymatgen.io.cif import CifParser
 import pyscf
 from pyscf.scf.uhf import UHF,mulliken_meta
 from copy import deepcopy
